58a7adb83a10d89ba588e0e819fae5ab2995ba0a
kowalski <cola23g@proton.me>
2025-11-10 00:20:58 +0100

    Update ppu.h

--- src/ppu.h
+++ src/ppu.h
@@ -11,7 +11,6 @@ typedef struct {
     uint16_t mode_clock;
     uint16_t line;
     uint32_t framebuffer[144][160];
-    uint32_t bg_palette[4], sprite_palette[2][4];
     uint8_t SCX, SCY, LCDC;
     uint8_t WX, WY;
     uint8_t frame_ready;
